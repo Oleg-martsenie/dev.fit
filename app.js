4ca77f17354d310ea9c15dd1a9aa39fb75ed13c3
João <povoajoao6@gmail.com>
2022-02-14 11:03:38 -0300

    Part 2 - Project ongoing

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const ModalButton = document.getElementById('modal-login');
 const ModalLogin = document.querySelector('.login-form');
+const MenuArea = document.querySelector('.navbar');
+const MobileMenu = document.getElementById('menu-mobile')
 
 ModalButton.onclick = () => {
     ModalLogin.classList.toggle('active')
+    MenuArea.classList.remove('active')
+}
+
+MobileMenu.onclick = () => {
+    ModalLogin.classList.remove('active')
+    MenuArea.classList.toggle('active')
 }
