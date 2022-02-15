a423e6f4de299655f6881eb1dfddc3a7d66926ec
João <povoajoao6@gmail.com>
2022-02-15 12:26:04 -0300

    Final Project | ⚠️🥑

---
+++ README.md
@@ -0,0 +1,15 @@
+## Dev.fit
+<hr/>
+
+## Technologies
+<kbd>HTML5</kbd>
+<kbd>CSS3</kbd>
+<kbd>JAVASCRIPT</kbd>
+
+## Libraries
+<h3>Font Awesome</h3>
+<h3>SwiperJs</h3> <span>To build a slide</span>
+
+## Gif
+
+<img src="\images\devfit.gif" alt="Presentation of project">
--- app.js
+++ app.js
@@ -12,3 +12,14 @@ MobileMenu.onclick = () => {
     ModalLogin.classList.remove('active')
     MenuArea.classList.toggle('active')
 }
+
+var swiper = new Swiper(".testimonials-slider", {
+    grapCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoHeight: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
